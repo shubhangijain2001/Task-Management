a471c4d44e1725bf1958cd79e31c9b7f82ec5ef2
fal4782 <falguni4782@gmail.com>
2023-11-02 16:04:45 +0530

    nav + sidebar and login backend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TasksComponent } from './tasks/tasks.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TasksComponent
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
